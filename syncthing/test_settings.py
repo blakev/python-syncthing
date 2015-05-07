0607584e869c12c6d8ca857abfa6579068d7d413
Blake VandeMerwe <blakev@null.net>
2015-05-07 16:57:17 -0600

    Additional tests and a settings file to run them independently.

---
+++ syncthing/test_settings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Created by: Blake on 5/7/2015 at 3:54 PM
+
+API_KEY = 'YOUR API KEY'
+PORT = 8080
+HTTPS = True
---
+++ test_syncthing.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Created by: Blake on 5/7/2015 at 3:42 PM
+
+import unittest
+
+from syncthing import Interface
+
+try:
+    from syncthing import private_settings as settings
+except ImportError:
+    from syncthing import test_settings as settings
+
+class SyncthingInterfaceTestCase(unittest.TestCase):
+    _multiprocess_can_split_ = True
+
+    def setUp(self):
+        self.i = Interface(None)
+        self.xi = Interface(settings.API_KEY, port=settings.PORT)
+
+    def test_interface_properties(self):
+        assert self.i.root
+
+    def test_interface_classmethods(self):
+        assert Interface.from_json
+        assert self.i.from_json
+        assert Interface.from_dict
+        assert self.i.from_dict
+
+    def test_interface_connected(self):
+        assert self.xi.is_connected
+
+    def test_interface_ping(self):
+        assert self.xi.req('/rest/system/ping', 'GET').ping == 'pong'
+        assert self.xi.req('/rest/system/ping', 'POST').ping == 'pong'
