c4cb26e3ae053e86b2cf8734abcfc9a4a53d0a0e
Blake VandeMerwe <blakev@null.net>
2015-05-07 16:57:54 -0600

    Improvements to class:Syncthing to require `force=True` for WARNING_METHODS

--- syncthing/syncthing.py
+++ syncthing/syncthing.py
@@ -4,29 +4,42 @@
 # Created by: Blake on 5/7/2015 at 12:59 PM
 
 import re
+from functools import wraps
 
+from six import with_metaclass
 from bunch import Bunch
 
-from .interface import Interface, VERB_SWAPS
-from .data import rest_md
+from interface import VERB_SWAPS as verb_swap
+from data import rest_md
 
 DOC_TOKEN = '###'
 CODE_BLOCK_REGEX = re.compile(r'```.*')
 REST_HOOK_REGEX = re.compile(r'###\W+(?P<verb>\S+)\W+(?P<endpoint>.+)', re.I)
 
 METHOD_STRING = '''
-{modifier}
+{modifiers}
 def {method}(self, **kwargs):
     """{docstring}"""
-    return self._interface.req("rest{endpoint}", "{verb}")'''.strip()
+    kwargs = self._check_method_warnings("{method}", kwargs)
+    return self._interface.req("rest{endpoint}", "{verb}", **kwargs)'''.strip()
+
+# instance-breaking methods defined in the REST-API
+WARNING_METHODS = [
+    'set_system_reset'
+]
 
 class SyncthingType(type):
     def _make_req(cls, method, endpoint, verb, docstring):
-        modifier = '@property' if verb == 'GET' else ''
-        return METHOD_STRING.format(
+        modifier = verb == 'GET'
+
+        modifiers = '@property' if modifier else ''
+
+        x = METHOD_STRING.format(
             docstring = docstring,
-            modifier = modifier, method = method,
+            modifiers = modifiers, method = method,
             endpoint = endpoint, verb = verb)
+
+        return x
 
     def __init__(cls, name, bases, clsdict):
         super(SyncthingType, cls).__init__(name, bases, clsdict)
@@ -56,7 +69,7 @@ class SyncthingType(type):
             docstring = re.sub(r'[\n]+', '\n', docstring)
 
             # create the method names
-            act_pieces = [VERB_SWAPS[verb]] + act.split('/')[1:]
+            act_pieces = [verb_swap[verb]] + act.split('/')[1:]
             act_method = '_'.join(filter(lambda x: x != '', act_pieces))
 
             # generate the code and add it to the object
@@ -65,13 +78,42 @@ class SyncthingType(type):
             setattr(cls, act_method, clsdict[act_method])
 
 
-class Syncthing(object, metaclass=SyncthingType):
+class Syncthing(with_metaclass(SyncthingType, object)):
     def __init__(self, interface):
         self._interface = interface
+        self._warning_methods = list(WARNING_METHODS)
+
+    def _check_method_warnings(self, method_name, kwargs_obj):
+        if method_name not in self._warning_methods:
+            return kwargs_obj
+
+        if not kwargs_obj.get('force', False):
+            raise UserWarning(method_name + ' is marked as a WARNING_METHOD, to call method pass \'force=True\'.')
+        else:
+            kwargs_obj.pop('force')
+            return kwargs_obj
+
+    def add_warning_method(self, method_name):
+        if method_name in self._warning_methods or method_name.startswith('__'):
+            return
+
+        if hasattr(self, method_name):
+            self._warning_methods.append(method_name)
+
+    def remove_warning_method(self, method_name):
+        if method_name in self._warning_methods:
+            self._warning_methods.remove(method_name)
 
     @property
-    def connected(self): return self._interface.is_connected
+    def connected(self):
+        return self._interface.is_connected
 
+    @property
+    def warning_methods(self):
+        return self._warning_methods
 
+    @property
+    def custom_warning_methods(self):
+        return set(self._warning_methods).difference(set(WARNING_METHODS))
 
 
