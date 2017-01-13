3bc10db19045c419f74afb50afb0e591e6558fac
Blake VandeMerwe <blakev@email.com>
2017-01-13 16:12:52 -0700

    (Fix #6) Test for scan folder call flow.
    Ensure there's no more `TypeError` in restructured API.

---
+++ tests/test_scan_folder.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# ~*~ coding: utf-8 ~*~
+# >>
+#   © Vivint, inc. 2017
+#   blake, python-syncthing
+# <<
+
+import os
+import unittest
+
+from six import string_types
+
+from syncthing import Syncthing, SyncthingError, BaseAPI
+
+KEY = os.getenv('SYNCTHING_API_KEY')
+HOST = os.getenv('SYNCTHING_HOST', '127.0.0.1')
+PORT = os.getenv('SYNCTHING_PORT', 8384)
+IS_HTTPS = bool(int(os.getenv('SYNCTHING_HTTPS', '0')))
+SSL_CERT_FILE = os.getenv('SYNCTHING_CERT_FILE')
+
+def syncthing():
+    return Syncthing(KEY, HOST, PORT, 10.0, IS_HTTPS, SSL_CERT_FILE)
+
+
+s = syncthing()
+
+class TestScanFolder(unittest.TestCase):
+    def test_folder_scan_root(self):
+        folders = s.stats.folder()
+        available = list(folders.keys())
+        if not available:
+            raise EnvironmentError('there are no folders to scan')
+        use = available[0]
+        ret = s.database.scan(use)
+        last_scan = folders[use]['lastScan']
+        assert isinstance(ret, string_types)
+        assert s.stats.folder()[use]['lastScan'] > last_scan
+
+    def test_folder_scan_sub(self):
+        last_scan = s.stats.folder()['default']['lastScan']
+        ret = s.database.scan('default', 'docs')
+        assert isinstance(ret, string_types)
+        assert s.stats.folder()['default']['lastScan'] > last_scan
